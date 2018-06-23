f76780dfe41ccb9247352695ef2851079bccc739
Adam Power <adam.power48@mail.dcu.ie>
2018-06-23 15:11:44 +0100

    Working basic neural Network. Custom Network for use with pre-generated weights.

--- mnist_test.py
+++ mnist_test.py
@@ -1,19 +1,15 @@
-from mnist import MNIST
-
-
-def squish_image(img):
-    return list(map(lambda v: v / 256, img))
-
+import mnist
+import scipy.misc
 
 if __name__ == "__main__":
+    images = mnist.train_images()
+    scipy.misc.toimage(scipy.misc.imresize(images[0, :, :] * -1 + 256, 10.))
+    x = images.reshape(images.shape[0], images.shape[1] * images.shape[2])
 
-    mndata = MNIST("samples")
+    train_images = mnist.train_images()
+    train_labels = mnist.train_labels()
 
-    images, labels = mndata.load_training()
+    test_images = mnist.test_images()
+    test_labels = mnist.test_labels()
 
-    img = squish_image(images[0])
-    for i in range(len(images[0])):
-        if i % 28 == 27:
-            print(img[i])
-        else:
-            print(end=str(img[i]) + "\t")
+    print(test_images[0])
--- network.py
+++ network.py
@@ -1,67 +1,79 @@
-import random
-from math import e
+import numpy as np
 
 
-def sigmoid(a):
-    return 1 / (1 + e ** -a)
+def sigmoid(x):
+    return 1.0 / (1 + np.exp(-x))
 
 
-# Mean Square Error
-def MS_sum_loss(desired, actual):
-    return sum(map(lambda x, y: (x - y) ** 2, desired, actual))
+def sigmoid_derivative(x):
+    return x * (1.0 - x)
 
 
-def MS_loss(desired, actual):
-    return list(map(lambda x, y: (x - y) ** 2, desired, actual))
+class Network:
+    # https://medium.com/@jamesloyys/here-you-go-9b3d91e2202e
+    def __init__(self, x, y):
+        self.input = x
+        self.weights1 = np.random.rand(self.input.shape[1], 4)
+        self.weights2 = np.random.rand(4, 1)
+        self.y = y
+        self.output = np.zeros(y.shape)
+
+    def feedforward(self):
+        self.layer1 = sigmoid(np.dot(self.input, self.weights1))
+        self.output = sigmoid(np.dot(self.layer1, self.weights2))
+
+    def backprop(self):
+        # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
+        d_weights2 = np.dot(self.layer1.T, (2 * (self.y - self.output) * sigmoid_derivative(self.output)))
+        d_weights1 = np.dot(self.input.T, (np.dot(2 * (self.y - self.output) * sigmoid_derivative(self.output),
+                                                  self.weights2.T) * sigmoid_derivative(self.layer1)))
+
+        # update the weights with the derivative (slope) of the loss function
+        self.weights1 += d_weights1
+        self.weights2 += d_weights2
 
 
-def diff_loss(desired, actual):
-    return list(map(lambda x, y: x - y, desired, actual))
+class CustomNetwork:
+    # For use with pre-generated weights
+    def __init__(self, weights, dims):
+        self.weights = weights
+        self.dims = dims
 
+    def feedforward(self, x):
+        h_layer = x
+        for i in range(len(self.dims) - 1):
+            # x2 = s(w1*x1 + b1)
+            h_layer = sigmoid(np.dot(h_layer, self.weights[i]))
 
-class Node:
-    def __init__(self):
-        self.weights = []
-        self.activation = 0
-
-    def calculate_activation(self, layer):
-        self.activation = sigmoid(sum(w * a for w, a in zip(self.weights, [n.activation for n in layer])))
+        return h_layer
 
 
 if __name__ == '__main__':
-    learning_rate = 0.01
-    inputs = [1, 0, 1, 0, 1]
-    desired_outputs = [0, 1, 0, 1, 0]
+    # Standard neural network
+    # 1: 0,0,1 -> 0
+    # 2: 0,1,1 -> 1
+    # 3: 1,0,1 -> 1
+    # 4: 1,1,1 -> 0
+    #
+    # X = np.array([[0, 0, 1],
+    #               [0, 1, 1],
+    #               [1, 0, 1],
+    #               [1, 1, 1]])
+    # y = np.array([[0], [1], [1], [0]])
+    #
+    # nn = Network(X, y)
+    #
+    # for i in range(10000):
+    #     nn.feedforward()
+    #     nn.backprop()
+    #
+    # print(nn.output)
 
-    layers = [[] for _ in range(4)]
-    for L in layers:  # Node creation
-        for i in range(5):
-            L.append(Node())
+    # Using pre-generated weights
+    dims = [3, 4, 4, 3]
+    weights = np.array([np.random.rand(dims[i], dims[i + 1]) for i in range(len(dims) - 1)])
+    net = CustomNetwork(weights, dims)
 
-    for i in range(1, len(layers)):  # Weight creation
-        for n in layers[i]:
-            n.weights = [random.uniform(-1, 1) for _ in range(len(layers[i - 1]))]
-
-    for i, n in enumerate(layers[0]):  # Inputs
-        n.activation = inputs[i]
-
-    for i in range(1, len(layers)):  # Activation calc
-        for n in layers[i]:
-            n.calculate_activation(layers[i - 1])
-
-    for L in layers:  # Print activations
-        for n in L:
-            print(end=str(n.activation) + "\t")
-        print()
-
-    loss_values = diff_loss(desired_outputs, [n.activation for n in layers[-1]])
-    # overall_loss = MS_sum_loss(desired_outputs, [n.activation for n in layers[-1]])
-    print("losses:\t", loss_values)
-    # print("overall loss:\t", overall_loss)
-
-    for i in range(len(layers) - 1, 0):
-        for n, w in zip(layers[i], loss_values):
-            node_loss = diff_loss(w, n.activation)
-            sum_node_weights = sum([abs(nw) for nw in w])
-            for old_w in n.weights:
-                loss = diff_loss()  # Calc loss for weights of each node
+    for _ in range(10):
+        x = np.random.rand(3)
+        print(x, net.feedforward(x))
